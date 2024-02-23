8fafd0814d2e73d0a5c0eb968e43a4c5f01f157e
Vince <b.mintheim@gmail.com>
2024-02-23 13:24:27 +0700

    commit

--- ar-webapp/src/App.js
+++ ar-webapp/src/App.js
@@ -2,10 +2,11 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React from 'react';
 import Header from './Header/Header';
+import Home  from './pages/Home/Home';
 import SignUp from './pages/SignUp/SignUp';
 import Login from './pages/Login/Login';
-import Subscription from './pages/Subscription/Subscription';
 import Footer from './Footer/Footer';
+import Subscription from './pages/Subscription/Subscription';
 <script src="https://kit.fontawesome.com/e57e6433be.js" crossorigin="anonymous"></script>
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
 
 
         <Switch>
+          <Route path="/home" component={Home}></Route>
           <Route path="/signup" component={SignUp}></Route>
           <Route path="/login" component={Login}></Route>
           <Route path="/sub-plan" component={Subscription}></Route>
