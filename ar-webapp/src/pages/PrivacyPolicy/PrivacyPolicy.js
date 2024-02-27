044d2cf7505c7603b84f38b7fa4e77f1c2fed8ed
ChoLwinThant <113784714+ChoLwinThant@users.noreply.github.com>
2024-02-27 09:49:47 +0630

    subscription and payment

---
+++ ar-webapp/src/pages/PrivacyPolicy/PrivacyPolicy.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class PrivacyPolicy extends Component {
+  render() {
+    return (
+      <div>PrivacyPolicy</div>
+    )
+  }
+}
+
+export default PrivacyPolicy
